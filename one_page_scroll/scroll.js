571abf8f0812b877687f8bbfc715699766ab1ee7
baekjinyeong <jinyeong1612@cttd.co.kr>
2018-02-01 18:02:33 +0900

    작업중

--- one_page_scroll/scroll.js
+++ one_page_scroll/scroll.js
@@ -2,4 +2,23 @@
 	'use strict';
 	console.log('test');
 
+	/*
+	1. Full일경우 window의 높이값을 구한다.
+	2. 버튼 클릭
+	3. window의 y값 이동
+	*/
+
+	let pageHeight = window.innerHeight; // window의 높이값
+	const pageSlide = document.querySelector('.btn-slide-page');
+	const pageSlideArr = pageSlide.querySelector('li');
+	const pageSlideLink = pageSlide.childNodes;
+
+
+	console.log(pageSlideLink);
+
+	pageSlideArr.addEventListener('click', e => {
+		e.target.classList.add("is-active");
+		console.log('a');
+	});
+
 }());
