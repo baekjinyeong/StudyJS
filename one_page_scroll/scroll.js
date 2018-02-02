3c66aa8499ccd8cbe307fc33e646a353aee1683c
baekjinyeong <jinyeong1612@cttd.co.kr>
2018-02-02 17:51:17 +0900

    작업중

--- one_page_scroll/scroll.js
+++ one_page_scroll/scroll.js
@@ -3,22 +3,38 @@
 	console.log('test');
 
 	/*
-	1. Full일경우 window의 높이값을 구한다.
-	2. 버튼 클릭
-	3. window의 y값 이동
+	1. 화면 전체 높이값을 구한다.
+	2. li의 index 값을 구한다.
+	3. li 클릭 시 index 값을 가져와 스크롤 이벤트를 준다.
+	4. 가져온 index 값을 클릭하면 화면 Y값을 이동시켜준다. [0] 일 때 높이값, [1] 일 때 높이값
 	*/
 
-	let pageHeight = window.innerHeight; // window의 높이값
-	const pageSlide = document.querySelector('.btn-slide-page');
-	const pageSlideArr = pageSlide.querySelector('li');
-	const pageSlideLink = pageSlide.childNodes;
+	let arr = [];
+	let doc = window.document;
 
+	const pageHeight = window.innerHeight; // window의 높이값
+	console.log(pageHeight); // 1014
 
-	console.log(pageSlideLink);
+	const pageButton = document.querySelectorAll("li");
+	let pageScrollTop = document.querySelector(".page-section").scrollTop; // 현재 top 값
+	console.log(`scrollTop값 : ${pageScrollTop}`); // 0
+	console.log(`버튼 인덱스값 : ${pageButton}`);
 
-	pageSlideArr.addEventListener('click', e => {
-		e.target.classList.add("is-active");
-		console.log('a');
-	});
+	// li 의 인덱스 값 가져오기
+	for (let i = 1; i < pageButton.length; i++) {
+		// console.log(i); // 0, 1
+		pageScrollTop = arr.push(`${pageButton[i] * pageHeight}`);
+		console.log(`scrollTop값 : ${pageScrollTop}`);
+		console.log(`버튼 인덱스값 : ${pageButton[i]}`);
 
+		// pageButton[i].addEventListener("click", e => {
+		// 	if (i === 0) {
+		// 		console.log("a");
+		// 		arr.push('${offsetTop}');
+		// 	} else {
+		// 		console.log("b");
+		// 	}
+		// });
+	}
+	console.log(arr);
 }());
