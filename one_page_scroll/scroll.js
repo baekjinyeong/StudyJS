a50ff122b790e27259f7c3f197b3b3864aca0ffa
baekjinyeong <jinyeong1612@cttd.co.kr>
2018-02-07 15:56:43 +0900

    섹션별 offsetTop값 찍어내기

--- one_page_scroll/scroll.js
+++ one_page_scroll/scroll.js
@@ -3,7 +3,7 @@
 	console.log('test');
 
 	/*
-	1. 화면 전체 높이값을 구한다.
+	1. 각 section별 scrollTop값을 구한다.
 	2. li의 index 값을 구한다.
 	3. li 클릭 시 index 값을 가져와 스크롤 이벤트를 준다.
 	4. 가져온 index 값을 클릭하면 화면 Y값을 이동시켜준다. [0] 일 때 높이값, [1] 일 때 높이값
@@ -11,23 +11,42 @@
 
 	let arr = [];
 
-	const scrollBtn = document.querySelectorAll('section-link');
-	const scrollHeight = document.body.scrollTop || document.documentElement.scrollTop; // 스크롤 높이값
-	let pageHeight = window.innerHeight;
-	console.log(`scrollHeight = ${scrollHeight}`);
-	console.log(`scrollBtn = ${scrollBtn}`);
-	console.log(`pageHeight = ${pageHeight}`);
 
-	for (let i = 1; i <= scrollBtn.length; i++) {
-		const scrollWindow = document.querySelector('.page-section');
-		let topPos = scrollWindow.offsetTop;
-		console.log(`topPos = ${topPos}`);
-		scrollBtn[i].addEventListener('click', e => {
-			if (topPos === 0) {
-				console.log("a");
-			} else if (topPos === 1) {
-				console.log("b");
-			}
-		});
+	function scrollIt (destination) {
+		const scrollHeight = document.body.scrollTop || document.documentElement.scrollTop; // 스크롤 높이값
+		const pageSection = document.querySelector('.page-section');
+		const pageHeight = window.innerHeight;
+		const pageOffset = window.pageYOffset;
+		const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+		console.log(`destinationOffset = ${destinationOffset}`);
 	}
+
+	function scroll (){
+		const scrollBtn = document.getElementsByTagName('li');
+		console.log(`scrollBtn = ${scrollBtn}`);
+		for (let i = 0; i < scrollBtn.length; i++) {
+			console.log(scrollBtn[i]);
+			
+			if (i === 0) {
+				scrollBtn[i].addEventListener('click', e => {
+					console.log("a");
+					scrollIt(document.querySelector('.page1'));
+				});
+			}
+			else if (i === 1) {
+				scrollBtn[i].addEventListener('click', e => {
+					console.log("b");
+					scrollIt(document.querySelector('.page2'));
+				});
+			}
+			else if (i === 2) {
+				scrollBtn[i].addEventListener('click', e => {
+					console.log("c");
+					scrollIt(document.querySelector('.page3'));
+				});
+			}
+		}
+	}
+	scroll();
 }());
+
