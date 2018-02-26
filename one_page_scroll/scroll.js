8526a00b3993f135dec502594e79d31a67104f27
baekjinyeong <jinyeong1612@cttd.co.kr>
2018-02-26 10:58:50 +0900

    페이지 이동 작업

--- one_page_scroll/scroll.js
+++ one_page_scroll/scroll.js
@@ -5,49 +5,29 @@
 
 
 	/*
-		1. 각 div태그의 top 위치 값을 구한다.
-		2. down, up 방향 조건을 설정해준다.
-		3. page 클릭 이벤트를 형성한다.
-		4. 버튼 클릭 시 해당 버튼의 인덱스 값을 부여해준다.
-		5. 클릭 버튼의 인덱스 값에 div 태그의 top 값을 곱해준다. (animate 사용)
+		1. 클릭 이벤트 형성
+		2. 버튼 인덱스 값 구하기
+		3. 클릭된 버튼 인덱스에 해당하는 페이지인덱스구하기
+		4. 구한 페이지 인덱스별 top 값 찾기
+		5. animate로 페이지 이동 - scrollTop
 	*/
 
 	function scrolling(){
-		var srollTop = $(window).scrollTop(),
-			windowHeight = $(window).height(),
-			listBtn = $('.btn-slide-page').find('li'),
-			listPageWrap = $('.wrap'),
-			listPage = $('.page-item'),
-			listPageLength = listPage.length,
-			moveDerection = 'down',
-			listIdx = $('.page-item').index(),
-			listTop = $('.page-item').offset().top;
+		var listBtn = $('.btn-slide-page').find('li').children('a'),
+			listPage = $('.page-list li');
 
 
-		// 스크롤 이동
-		var scrollMove = function(){
-			if(moveDerection === 'down') {
-				$(window).stop().animate({scrollTop : listIdx * listTop},300);
+		// 클릭 이벤트
+		$(listBtn).on('click', function(e) {
+			event.preventDefault();
 
-			} else if((moveDerection === 'up')){
-				$(window).stop().animate({scrollTop : listIdx * listTop},300);
-			}
-		};
+			var btnIdx = $(this).parent('li').index(),
+				offsetTop = $(listPage).eq(btnIdx).offset().top;
 
-		// 스크롤 이벤트
-		var scrollEvent = function(){
-			$('.btn-slide-page li').on('click', function(){
-				var btnIdx = $(this).index();
-				if(listIdx < btnIdx ) {
-					moveDerection = 'down';
-				} else {
-					moveDerection = 'up';
-				}
-				console.log(listIdx, btnIdx, moveDerection, listTop);
-			});
-		};
-		scrollEvent();
-		
+			$('body, html').stop().animate({scrollTop: offsetTop},300);
+			console.log(btnIdx, offsetTop);
+		});
 	};
+
 	scrolling();
 }());
