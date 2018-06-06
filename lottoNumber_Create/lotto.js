63f515d8787c9c7135587a9e81db6593682848ee
baekjinyeong <jinyeong1612@cttd.co.kr>
2018-06-07 08:56:21 +0900

    로또번호생성기 중복 제거 작업중

--- lottoNumber_Create/lotto.js
+++ lottoNumber_Create/lotto.js
@@ -15,7 +15,8 @@
 
 	function lotto(){
 		var lottoArray = [], // 1 ~ 45 번호를 저장
-			numberIdx = 0, // 생성된 번호
+			numberIdx = 1, // 생성된 번호
+			numberArray = [], // 생성된 번호를 저장
 			count = 0, // 추첨된 로또 번호 갯수
 			button = document.querySelector('.button'),
 			print = document.querySelector('.lottoWrap');
@@ -26,7 +27,7 @@
 		for (var i = 1; i <= 45; i++) {
 			lottoArray.push(i);
 		};
-		console.log(lottoArray);
+		// console.log(lottoArray);
 
 		// 랜덤 번호 섞기
 		
@@ -39,19 +40,29 @@
 				// 랜덤번호 생성
 				for (var i = 0; i <= 6; i++) {
 					for (var k = 0; k <= i; k++) {
-						numberIdx = parseInt(Math.random() * lottoArray.length);
-						/* 전에 뽑았던 배열과 지금 뽑은 배열의 값을 비교하여 두 수가 같다면 배열수를 1감소시켜 다시 그 배열부터 채우게 한다.*/
+						numberIdx = parseInt((Math.random() * lottoArray.length) + 1);
 					}
 				}
-				arr.push("<td>" + lottoArray[numberIdx] + "</td>");
-				
+				overlapNum();
 			} else {
 				arr.push("<tr>");
+				numberArray = [];
 				count = 0;
 			}
-			console.log(lottoArray[numberIdx]);
+			console.log(numberArray,numberArray.slice(-1)[0]);
 			print.innerHTML = arr.join('');
 		});
+
+
+		/* 중복확인 - 마지막 배열값과 이전 배열값을 비교 후 동일하면 다시 뽑는다. */
+		function overlapNum(){
+			if(numberArray.slice(-1)[0] !== numberArray) {
+				numberArray.push(numberIdx);
+				arr.push("<td>" + numberIdx + "</td>");
+			} else {
+
+			}
+		}
 	};
 	lotto();
 	
