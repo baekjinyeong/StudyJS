e37bb0bea0ac811e9ec5234ea9a4cb60f3e2d566
baekjinyeong <jinyeong1612@cttd.co.kr>
2018-07-12 18:02:05 +0900

    작업

--- visualSlide/slide.js
+++ visualSlide/slide.js
@@ -6,29 +6,38 @@ function slide(){
 
 	var guideList = $('.guide-slide-list'),
 			guideItem = guideList.find('.guide-slide-item'),
+			guideLength = guideItem.length -1,
 			guideWidth = guideItem.outerWidth(),
 			count = 0;
 
-	$(document).on('click','btn-slider-prev', function(){
-		motion(prev);
+	$(document).on('click','.btn-slider-prev', function(){
+		motion('prev');
 	});
 
-	$(document).on('click','btn-slider-next', function(){
-		motion(next);
+	$(document).on('click','.btn-slider-next', function(){
+		motion('next');
 	});
-
-	function move(){
-		guideList.stop().animate({'margin-left': count * (-guideWidth) + 'px'},300);
-	}
 
 	function motion(action){
-		if(action === next) {
+		if(action == 'next' && count < guideLength) {
 			count++;
-		} else {
+			if(count >= guideLength) {
+				count = guideLength;
+			}
+		} else if (action == 'prev') {
 			count--;
+			if(count <= 0) {
+				count = 0;
+			}
 		}
 		move();
 	};
+
+	function move(){
+		guideList.stop().animate({
+			'margin-left': count * (-guideWidth) + 'px'
+		},300);
+	}
 };
 slide();
 
