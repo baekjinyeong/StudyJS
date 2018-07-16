72b07902596aca1752b04699c88c8775baf5c954
baekjinyeong <jinyeong1612@cttd.co.kr>
2018-07-16 11:03:12 +0900

    1차

--- visualSlide/slide.js
+++ visualSlide/slide.js
@@ -10,20 +10,37 @@ function slide(){
 			guideWidth = guideItem.outerWidth(),
 			count = 0;
 
-	$(document).on('click','.btn-slider-prev', function(){
-		motion('prev');
+	$(document).on('click','.btn-slider-prev', function(e){
+		e.preventDefault();
+		direction('prev');
 	});
 
-	$(document).on('click','.btn-slider-next', function(){
-		motion('next');
+	$(document).on('click','.btn-slider-next', function(e){
+		e.preventDefault();
+		direction('next');
 	});
 
-	function motion(action){
-		if(action == 'next' && count < guideLength) {
+	$(document).on('click', '.btn-data-slides', function(e){
+		e.preventDefault();
+		var target = $(e.target),
+				current = target.parent('.data-slides-item'),
+				btnIdx = current.index();
+
+		count = btnIdx;
+		$(current).addClass('is-active').siblings().removeClass('is-active');
+
+		guideList.stop().animate({
+			'margin-left' : $(current).index() * -guideWidth
+		},300);
+	});
+
+	function direction(action){
+		if(action == 'next') {
 			count++;
 			if(count >= guideLength) {
 				count = guideLength;
 			}
+
 		} else if (action == 'prev') {
 			count--;
 			if(count <= 0) {
@@ -37,6 +54,14 @@ function slide(){
 		guideList.stop().animate({
 			'margin-left': count * (-guideWidth) + 'px'
 		},300);
+		currentPage();
+	}
+
+	function currentPage(){
+		var pageIdx = guideItem.eq(count).index();
+		$('.btn-data-slides').closest('.data-slides-item').eq(pageIdx).addClass('is-active').siblings().removeClass('is-active');
+
+		console.log(pageIdx,count);
 	}
 };
 slide();
